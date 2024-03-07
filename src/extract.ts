6b13063c1b56d0d74b74944ec39dc86aef9a1dad
Fethca <jay.grijol@gmail.com>
2024-03-07 11:13:50 +0100

    fix: extract version with ESM

--- src/extract.ts
+++ src/extract.ts
@@ -1,6 +1,9 @@
+import { readFileSync } from 'fs'
 import { resolve } from 'path'
 
 export async function extractVersion(): Promise<string> {
-  const pjson = await import(resolve(process.cwd(), 'package.json'))
+  const path = resolve(process.cwd(), 'package.json')
+  const pckage = readFileSync(path, 'utf-8')
+  const pjson = JSON.parse(pckage)
   return pjson.version
 }
